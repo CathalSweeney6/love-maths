3ff50dfc2e3dc22224de16f149550f785a7b04ee
CathalSweeney6 <>
2023-05-05 16:41:24 +0000

    Added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unkown Game Type: ${gameType}`);
         throw `Unkown Game Type: ${gameType}. Aborting!`;
@@ -64,6 +66,8 @@ function calculateCorrectAnswer() {
 
     if (operator === '+') {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -98,6 +102,8 @@ function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1; 
+    document.getElementById('operand2').textContent = operand2; 
+    document.getElementById('operator').textContent = 'x'; 
 }
